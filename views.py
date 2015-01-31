c7cb87ab2f5e9631f94ec8c617be9daea44e03d9
Chris Barrett <chris@codesaru.com>
2015-01-30 18:58:33 -0800

    Added hard-coded story.
    There's only one story, and has only been for three years.  Not creating
    a whole system for something there's only one of.

--- views.py
+++ views.py
@@ -1,10 +1,14 @@
-from flask import flash, render_template
-from app import app
+from flask import abort, flash, render_template
+from app import app, stories
 
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template('index.html')
+
+@app.route('/story/account-less-online-friend-oriented-leaderboards')
+def story_account_less_leaderboards():
+	return render_template('story/account-less-online-friend-oriented-leaderboards.html')
 
 @app.route('/test')
 def test():
